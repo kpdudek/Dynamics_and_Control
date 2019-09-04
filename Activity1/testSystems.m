1ddf1eb090b64ac1f9a0038b8e2f3987466e02c3
Kurt <kpdudek2@gmail.com>
2019-09-04 09:17:10 -0400

    Class 1

---
+++ Activity1/testSystems.m
@@ -0,0 +1,29 @@
+%=======================================================================
+% testSystems.m
+%
+%   Template file for creating inputs and using blackBox.m
+%=======================================================================
+
+%%
+% Let's make a time vector
+dt = .05;
+T = 10;
+t = 0:dt:T;
+
+%%
+% Let's make a unit step
+startLoc = 0.25;        % Should be in [0,1] and is fraction 
+u_1 = zeros(size(t));
+u_1(floor(startLoc*length(u_1)):end) = 1;
+
+% Throw that input into the blackBox function for model 1
+y_1 = blackBox(t,u_1,1,1);
+
+%%
+% Now a sinusoidal input
+f = 50;                 % Frequency in Hz
+u_2 = zeros(size(t));
+u_2 = sin(2*pi*f*t);
+
+% Throw that input into the blackBox function for model 1
+y_2 = blackBox(t,u_2,1,2);
