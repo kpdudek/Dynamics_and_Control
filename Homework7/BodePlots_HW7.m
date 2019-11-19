9a8dd8d47fb33edea6376a441f9569d84818bc28
Kurt <kpdudek2@gmail.com>
2019-11-18 22:01:27 -0500

    HW 7

---
+++ Homework7/BodePlots_HW7.m
@@ -0,0 +1,8 @@
+myopt = bodeoptions;
+myopt.Grid = 'on'; myOpt.FreqUnits = 'Hz';
+myopt.XLabel.FontSize = 16; myopt.YLabel.FontSize = 16;
+myopt.TickLabel.FontSize = 16;
+myopt.Title.String = '';
+set(findall(gcf,'Type','line'),'LineWidth',2)
+
+
